c65dedb253e74f3bdf86fbd5b3a442d433cb721c
hussedev <husse.dev@gmail.com>
2025-05-08 17:02:43 +0700

    implemented withdraw function

--- FundMe.sol
+++ FundMe.sol
@@ -21,8 +21,30 @@ contract FundMe {
         msg.value.getConversionRate();
         require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough eth"); // 1e18 = 1 ETH
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    // function withdraw() public {}
+    function withdraw() public {
+        // reset mapping
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            addressToAmountFunded[funders[funderIndex]] = 0;
+        }
+        //reset array
+        funders = new address[](0);
+
+        // msg.sender -> type address
+        // we need type payable address
+
+        // We have 3 options for withdrawal
+        // // with transfer
+        // payable(msg.sender).transfer(address(this).balance); 
+
+        // // with send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);  
+        // require(sendSuccess, "send failed");  
+        
+        // with call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "call failed");  
+    }
 }
